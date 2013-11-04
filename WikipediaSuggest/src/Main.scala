6c2e23acfb6dc70693c8609f5a8ea746cd4022ae
headinthebox <emeijer@applied-duality.com>
2013-11-04 19:53:15 +0100

    Before restarting IntelliJ

--- WikipediaSuggest/src/Main.scala
+++ WikipediaSuggest/src/Main.scala
@@ -1,10 +1,13 @@
+import observablex.Scheduler
+import search.Search
+
 object Main {
 
   def main(args: Array[String]): Unit = {
 
-    //implicit val s = Scheduler.NewThreadScheduler
+   implicit val s = Scheduler.NewThreadScheduler
 
-    //Search.quakesWithCountry().subscribe(onNext = x => println(x))
+    Search.quakesWithCountry().subscribe(onNext = x => println(x))
 
 
     //gui.Swing.startup(args)
