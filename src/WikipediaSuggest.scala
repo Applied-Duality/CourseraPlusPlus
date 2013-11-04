0feeff283f27b4ae3731c00e28c153892dd573cb
headinthebox <emeijer@applied-duality.com>
2013-11-04 16:51:46 +0100

    Added GUI

--- src/WikipediaSuggest.scala
+++ src/WikipediaSuggest.scala
@@ -5,9 +5,12 @@ import search.Search
 object WikipediaSuggest {
   def main(args: Array[String]): Unit = {
 
-    implicit val s = Scheduler.NewThreadScheduler
+    //implicit val s = Scheduler.NewThreadScheduler
 
-    Search.quakesWithCountry().subscribe(onNext = x => println(x))
+    //Search.quakesWithCountry().subscribe(onNext = x => println(x))
+
+
+    //gui.Swing.startup(args)
 
     readLine()
     println("bye")
---
+++ src/gui/Reaction.scala
@@ -0,0 +1,9 @@
+package gui
+
+import scala.swing.Reactions.Reaction
+
+object Reaction {
+  def apply(r: Reaction) : Reaction = { r }
+}
+
+
